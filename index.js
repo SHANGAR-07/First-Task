b6509fc6ae924255986b3f61f7df62998b9ac7fb
SRISHANGAR M <88281417+SHANGAR-07@users.noreply.github.com>
2022-12-28 18:09:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+function onclick(){
+    var inputname = document.getElementById("name");
+    var inputemail = document.getElementById("email");
+    var inputpassword = document.getElementById("passord");
+}
